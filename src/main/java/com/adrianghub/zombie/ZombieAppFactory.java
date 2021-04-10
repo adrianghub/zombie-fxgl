8216becd9206581250305f85f5ba9e5896fe5e5e
Adrian <adrian_zinko@op.pl>
2021-04-10 10:57:47 +0200

    Feat: Spawn bullets

--- src/main/java/com/adrianghub/zombie/ZombieAppFactory.java
+++ src/main/java/com/adrianghub/zombie/ZombieAppFactory.java
@@ -1,12 +1,17 @@
 package com.adrianghub.zombie;
 
 import com.adrianghub.zombie.components.SurvivorComponent;
+import com.almasb.fxgl.dsl.components.OffscreenCleanComponent;
+import com.almasb.fxgl.dsl.components.ProjectileComponent;
 import com.almasb.fxgl.dsl.components.RandomMoveComponent;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.EntityFactory;
 import com.almasb.fxgl.entity.SpawnData;
 import com.almasb.fxgl.entity.Spawns;
+import javafx.geometry.Point2D;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 import static com.adrianghub.zombie.ZombieApp.EntityType.*;
 import static com.almasb.fxgl.dsl.FXGL.*;
@@ -33,6 +38,19 @@ public class ZombieAppFactory implements EntityFactory {
                 .with(new RandomMoveComponent(
                         new Rectangle2D(0, 0,
                                 getAppWidth()+100, getAppHeight()+100), 150))
+                .build();
+    }
+
+    @Spawns("bullet")
+    public Entity newBullet(SpawnData data) {
+        Point2D direction = data.get("dir");
+
+        return entityBuilder(data)
+                .type(BULLET)
+                .viewWithBBox("bullet.png")
+                .collidable()
+                .with(new ProjectileComponent(direction, 1000))
+                .with(new OffscreenCleanComponent())
                 .build();
     }
 }
